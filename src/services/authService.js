e997c2704a3b572410eb8434fd30e236fcb25ad5
PINNINTI TEJA SREE <152084449+tejasree5@users.noreply.github.com>
2025-09-19 19:44:59 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://35.154.47.218:8081/auth";
+const API_URL = "http://13.53.168.76:8081/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
