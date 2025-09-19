f6e4f1980bf82e40549cd6d3c4b03922abff32c0
PINNINTI TEJA SREE <152084449+tejasree5@users.noreply.github.com>
2025-09-19 19:46:02 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://35.154.47.218:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://13.53.168.76:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `http://35.154.47.218:8081/api/products/images/${imagePath}`;
+  return `http://13.53.168.76:8081/api/products/images/${imagePath}`;
 };
