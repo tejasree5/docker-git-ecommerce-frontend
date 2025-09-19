f8d46b181a1f103129736b77c22963d5a47ae1fc
tejasreepinninti5@gmail.com <tejasree5@gmail.com>
2025-09-19 18:32:48 +0530

    first commit

---
+++ src/services/authService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_URL = "http://35.154.47.218:8081/auth";
+
+export const login = async (username, password) => {
+  const response = await axios.post(`${API_URL}/login`, { username, password });
+  localStorage.setItem("token", response.data);
+  return response.data;
+};
+
+export const signup = async (username, email, password) => {
+  return axios.post(`${API_URL}/signup`, { username, email, password });
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+};
